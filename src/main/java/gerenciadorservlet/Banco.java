bc29f3b4428a5cd3a457d2caa27b2e028a05618b
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2023-10-02 18:11:01 -0300

    Criar bloco static para ter duas empresas carregadas no Banco

--- src/main/java/gerenciadorservlet/Banco.java
+++ src/main/java/gerenciadorservlet/Banco.java
@@ -4,7 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Banco {
+	
 	private static List<Empresa> listaEmpresas = new ArrayList<>();
+	
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setNome("Avanade");
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("Microsoft");
+		listaEmpresas.add(empresa);
+		listaEmpresas.add(empresa2);
+		
+	}
+	
 
 	public void adicionar(Empresa empresa) {
 		
